b283138bd7fa8b17fe161c88726de47186d92a05
Ramón <55927337+Ramon3210@users.noreply.github.com>
2021-10-13 12:08:33 -0500

    Update Menu.jsx

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -4,8 +4,6 @@ import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
 
-import useScript from 'hooks/useScript';
-
 class Menu extends Component {
   render() {
     return (
@@ -16,10 +14,6 @@ class Menu extends Component {
       </div>
     );
   }
-}
-
-const MyComponent = props => {
-  useScript('//code.tidio.co/kwzqcn7i5i3zegf54pdewop87bzwcvhk.js');
 }
 
 export default Menu;
